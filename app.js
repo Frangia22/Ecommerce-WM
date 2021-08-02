3d46d2509e26bb621a0aea6f310b16d7099143f6
Frangia22 <giacherofranc22@gmail.com>
2021-08-01 21:27:59 -0300

    Agrego las opciones de session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +14,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Session
+app.use(session({
+  secret: 'Gordini wines club',
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 30000 }
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
